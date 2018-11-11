4904c3a6f32d92ee6e41eeec7f352a7cfec201e1
Matt <mknickelbine@gmail.com>
2018-11-11 13:03:26 -0600

    testing tv interaction-2

--- public/js/tv-component.js
+++ public/js/tv-component.js
@@ -1,3 +1,12 @@
-// window.document.querySelector('#tv-model').addEventListener('click', function (evt) {
-//   console.log('The TV was clicked!!!!!!!');
-// });
+AFRAME.registerComponent('tv-component', {
+  dependencies: ['raycaster'],
+
+  init: function () {
+    var tv = window.document.createElement('a-entity');
+    tv.setAttribute('mixin', 'tv-model');
+    this.el.addEventListener('raycaster-intersected', function () {
+      playVideo();
+    });
+    this.el.appendChild(tv);
+  }
+});
