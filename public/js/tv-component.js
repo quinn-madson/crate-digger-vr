cf7b919265a88efb4964a6a8920ce8e3c2626bc9
Quinn Madson <quinn@3data.io>
2018-11-11 14:18:19 -0600

    Shortened lazer pointer; Added cool down time to tv interaction.

--- public/js/tv-component.js
+++ public/js/tv-component.js
@@ -5,13 +5,21 @@ AFRAME.registerComponent('tv-component', {
     var tv = window.document.createElement('a-entity');
     var video = window.document.querySelector('#music-video');
     tv.setAttribute('mixin', 'tv-model');
+    var that = this;
     this.el.addEventListener('raycaster-intersected', function () {
-      if (video.seeking) {
-        pauseVideo();
-      } else {
-        playVideo();
+      if (!that.lock) {
+        that.lock = true;
+        if (!video.paused) {
+          pauseVideo();
+        } else {
+          playVideo();
+        }
+        setTimeout(function() {
+          that.lock = false;
+        }, 2000)
       }
     });
     this.el.appendChild(tv);
-  }
+  },
+  lock: false
 });
